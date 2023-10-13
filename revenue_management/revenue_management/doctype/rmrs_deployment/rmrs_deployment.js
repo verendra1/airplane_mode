c5375ec04c0e02f4797300489c412f3c1d36227a
chaitanya <krishnachaitanya.k@caratred.com>
2023-10-13 18:17:25 +0530

    added cutomizations to marsha deatils and added query report

--- revenue_management/revenue_management/doctype/rmrs_deployment/rmrs_deployment.js
+++ revenue_management/revenue_management/doctype/rmrs_deployment/rmrs_deployment.js
@@ -2,7 +2,82 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('RMRS Deployment', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		console.log("Triggred")
+	},
+	deployement_type:function(frm){
+		
 
-	// }
+	},
+	cluster_name:function(frm){
+		if (frm.doc.deployment_type){
+			frm.doc.hotel_name = ''
+			get_details(frm)
+			
+		}
+	},
+	hotel_name:function(frm){
+		if (frm.doc.deployment_type){
+			frm.doc.cluster_name = ''
+			get_details(frm)
+
+		}
+	},
 });
+
+function reset_child_table_data(frm){
+
+}
+
+
+function get_details(frm){
+	let child_table = frm.doc.properties;
+	if (child_table){
+		for (var i = 0; i < child_table.length; i++) {
+			frappe.model.clear_doc(child_table[i].doctype, child_table[i].name);
+		}
+	}
+	
+	// Refresh the form to reflect the changes
+	frm.refresh();
+
+	if (frm.doc.cluster_name != ''){
+		frappe.db.get_list(
+			"Marsha Details", {
+				filters:{'cluster': frm.doc.cluster_name}, 
+				fields:['name','property_name','cluster','region',
+				'city','currency','area','sub_region','country']}
+		).then((res) => {
+			res.forEach((marsha) => {
+				// options.push(party_type.name);
+				var child = frm.add_child("properties");
+				child.marsha = marsha.name;
+				child.property_name = marsha.property_name;
+				child.currency = marsha.currency;
+
+			});
+			refresh_field("properties");
+
+		});
+	}else{
+		frappe.db.get_list(
+			"Marsha Details", {
+				filters:{'name': frm.doc.hotel_name}, 
+				fields:['name','property_name','cluster','region',
+				'city','currency','area','sub_region','country']}
+		).then((res) => {
+			res.forEach((marsha) => {
+					var child = frm.add_child("properties");
+				 	child.marsha = marsha.name;
+				 	child.property_name = marsha.property_name;
+					 child.currency = marsha.currency;
+
+
+	
+			});
+			refresh_field("properties");
+
+		});
+	}
+	
+}
--- revenue_management/revenue_management/doctype/rmrs_deployment/rmrs_deployment.py
+++ revenue_management/revenue_management/doctype/rmrs_deployment/rmrs_deployment.py
@@ -3,17 +3,48 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
+
 
 class RMRSDeployment(Document):
-	pass
+    pass
 
 
 def update_name_field(doc, method=None):
-	try:
-		if doc.deployment_type == "Cluster":
-			doc.hotel_cluster_name = doc.cluster_name
-		if doc.deployment_type == "Individual Property":
-			doc.hotel_cluster_name = doc.hotel_name
-		print(doc.hotel_cluster_name)
-	except Exception as e:
-		frappe.log_error(str(e), "update_name_field")
+    try:
+        if doc.deployment_type == "Cluster":
+            doc.hotel_cluster_name = doc.cluster_name
+        if doc.deployment_type == "Individual Property":
+            doc.hotel_cluster_name = doc.hotel_name
+        print(doc.hotel_cluster_name)
+    except Exception as e:
+        frappe.log_error(str(e), "update_name_field")
+
+
+# @frappe.whitelist()
+# def get_properties(deployment_type="",cluster_name="",property_name=""):
+#     '''
+#     get properties from marsha details based on cluster/Hotel
+#     if customer choosed cluster return all marsha details under cluster
+#         if customer selected individual hotel return singal marsha details
+
+#     '''
+#     try:
+#         if deployment_type == 'Cluster':
+#             marsha_details = frappe.db.get_list('Marsha Details', filters={
+#                 'cluster': cluster_name,
+#                 # 'fields':['*']
+#             },)
+#             return {"success":True,"data":marsha_details}
+
+#         else:
+#             marsha_details = frappe.db.get_list('Marsha Details', filters={
+#                 'property_name': property_name,
+#                 # 'fields':['*']
+#             },)
+#             return {"success":True,"data":marsha_details}
+
+#     except Exception as e:
+#         print(e)
+#         return {"success":False,"data":{}}
+
