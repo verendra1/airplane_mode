14c0d075f28585bcb45e28fc36922d9d3747bf0d
prasanth <prasanth@caratred.com>
2023-11-06 09:53:36 +0530

    changed data_import method

--- revenue_management/utlis.py
+++ revenue_management/utlis.py
@@ -1,7 +1,7 @@
 import frappe
 import traceback
 import sys
-from frappe.core.doctype.data_import.data_import import form_start_import
+from frappe.core.doctype.data_import.data_import import start_import
 
 
 def dataimport(file=None, import_type=None, reference_doctype=None):
@@ -9,15 +9,16 @@ def dataimport(file=None, import_type=None, reference_doctype=None):
         doc = {
                 "doctype": "Data Import",
                 "reference_doctype": reference_doctype,
-                "import_type": import_type,
-                "import_file": file,
+                "import_type": "Insert New Records",
                 "status": "Pending"
             }
         data = frappe.get_doc(doc)
         data.insert()
         frappe.db.commit()
+        frappe.db.set_value("Data Import", data.name, "import_file", file)
+        frappe.db.commit()
         data_import = data.name
-        start_import = form_start_import(data_import)
+        start_import = start_import(data_import)
         return {"success": True}
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
